927b08f5b60a5f7cc03722f844484ac108b4dd8c
AreyouKewpie <ussie_uk@yahoo.co.jp>
2018-08-09 22:26:13 +0900

    データベース挑戦

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,8 @@ class HomeController < ApplicationController
 
   def answer
 
-    @restaurants = ["楽楽楽","にぼじろう","キラメキ","RAJU","松之助","ルネ","ケニア","JK","ハイライト","よし","おくだ","方円美味","味香園","こあのすけ","アオゾラ"]
-    tenposu = @restaurants.length
-    @number = rand(tenposu)
-    @the_restaurant = @restaurants[@number]
+    tenposu = Post.all.length
+    @number = rand(tenposu-1)
+    @the_restaurant = Post.all[@number+1].name
   end
 end
