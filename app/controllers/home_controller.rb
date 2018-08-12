f40b546f6a87ec03b00858deaa156f5829e680ea
AreyouKewpie <ussie_uk@yahoo.co.jp>
2018-08-12 14:29:03 +0900

    コメント表示機能

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,6 @@ class HomeController < ApplicationController
     tenposu = Post.all.length
     @number = rand(tenposu)
     @the_restaurant = Post.all[@number-1].name
+    @the_comment = Post.all[@number-1].comment
   end
 end
